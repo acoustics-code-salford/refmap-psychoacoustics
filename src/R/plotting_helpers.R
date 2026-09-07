bda83fa5571f397da7ebd5cddb09f9bd329d412b
Mike Lotinga <m_lotinga@yahoo.co.uk>
2026-09-07 14:23:14 +0100

    updated analyses

--- src/R/plotting_helpers.R
+++ src/R/plotting_helpers.R
@@ -3,25 +3,22 @@ require(ggplot2)
 
 # Rescale ggplot tick marks for variables --------------------------------------
 
-rescaleTicks <- function(dataVar, dataVarScl, sep=3, ax='x'){
+rescaleTicks <- function(dataVar, dataVarScl, sep=3, ax='x', sd.scale=FALSE){
+
+  data_mean <- mean(dataVar, na.rm=TRUE)
+  scale_factor <- if (sd.scale) sd(dataVar, na.rm=TRUE) else 1
+  
+  dataVar_min <- min(dataVar, na.rm=TRUE)
+  dataVar_max <- max(dataVar, na.rm=TRUE)
+  
+  ticks <- seq(floor(dataVar_min), ceiling(dataVar_max), by=sep)
+  
+  breaks <- (ticks - data_mean) / scale_factor
   
   if (ax == 'x'){
-    outAx <- scale_x_continuous(breaks=seq(floor(min(dataVar)),
-                                           ceiling(max(dataVar)),
-                                           by=sep) -
-                                  (max(dataVar) - max(dataVarScl)),
-                                labels=seq(floor(min(dataVar)),
-                                           ceiling(max(dataVar)),
-                                           by=sep))
-    
+    outAx <- scale_x_continuous(breaks=breaks, labels=ticks)
   } else if (ax == 'y'){
-    outAx <- scale_y_continuous(breaks=seq(floor(min(dataVar)),
-                                           ceiling(max(dataVar)),
-                                           by=sep) -
-                                  (max(dataVar) - max(dataVarScl)),
-                                labels=seq(floor(min(dataVar)),
-                                           ceiling(max(dataVar)),
-                                           by=sep))
+    outAx <- scale_y_continuous(breaks=breaks, labels=ticks)
   } else {
     stop("Invalid axis argument")
   }
